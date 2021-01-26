8c34ca06883f47cc7e915fc3b45d846ef1d58d4f
Eugene Savich <zh_savich@mail.ru>
2021-01-26 21:44:07 +0300

    Second Commit

---
+++ src/Functions/Calculator.java
@@ -0,0 +1,7 @@
+package Functions;
+public class Calculator {
+    public static void main(String[] args) {
+        FinalAutomate finalAutomate = new FinalAutomate();
+        finalAutomate.CalculateAutomate(4);
+    }
+}
